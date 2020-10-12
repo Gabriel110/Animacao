5b5f218cc44e12ab892159aff7bfa65832325fa6
Gabriel1101 <gabrielcorreiadev@gmail.com>
2020-10-11 22:35:02 -0300

    Class responsavel por rederizar sprites

---
+++ game/Spritesheet.js
@@ -0,0 +1,37 @@
+class Spritesheet{
+  constructor(context, imagem, linha, coluna){
+    this.context = context;
+    this.imagem = imagem;
+    this.numeroLinha = linha;
+    this.numeroColuna = coluna;
+    this.intervalo = 0;
+    this.linha = 0;
+    this.coluna = 0;
+  }
+  proximoQuadro(){
+    let agora = new Data().getTime();
+    if(!this.ultimoTempo) this.ultimoTempo = agora;
+    if(agora - this.ultimoTempo < this.intervalo) return;
+    if(this.coluna < this.numeroColuna-1)
+      this.coluna++;
+    else
+      this.coluna = 0;
+    this.ultimoTempo = agora;
+  }
+  desenhar(x,y){
+    let larguraQuadro = this.imagem.width/this.numeroColuna;
+    let alturaQuadro = this.imagem.height/this.numeroLinha;
+
+    this.context.drawImage(
+      this.imagem,
+      larguraQuadro*this.coluna,
+      alturaQuadro*this.linha,
+      larguraQuadro,
+      alturaQuadro,
+      x,
+      y,
+      larguraQuadro,
+      alturaQuadro
+    )
+  }
+}
