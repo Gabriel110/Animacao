46603d77bc598f6870fa5aaa5340905fffd69866
Gabriel110 <gabrielcorreiadev@gmail.com>
2022-03-30 18:42:18 -0300

    adicionando mais planetas

--- sistema_solar/com_lib/sketch.js
+++ sistema_solar/com_lib/sketch.js
@@ -1,21 +1,25 @@
 let sol
-let planeta
+let mercurio 
+let venus
+let terra
+let marte
+let jupter
+let saturno
+let urano
+let neturno
 
 let windowW = 900
 let windowH = 900
 let lateral = 100
-let GRAVIDADE = 2
+let GRAVIDADE = 4
 
 function setup() {
   createCanvas(windowW,windowH)
 
   sol = new Corpo(200, createVector(0, 0), createVector(0, 0))
-  let raio = random(sol.raio, min((windowW/2)+lateral, (windowH/2)-lateral))
-  let teta = random(TWO_PI)
-
-  let posicaoPlaneta = createVector(raio*cos(teta), raio*sin(teta))
- 
-  planeta = new Corpo(25, posicaoPlaneta, createVector(1, 0))
+  mercurio = new Corpo(25, createVector(1, -150), createVector(2.3, 0))
+  venus = new Corpo(30, createVector(1, -200), createVector(2, 0))
+  terra = new Corpo(35, createVector(1, -250), createVector(1.8, 0))
   
 }
 
@@ -23,11 +27,19 @@ function draw() {
   
   translate(width/2, height/2);
   background(180)
-  planeta.desenhar()
-  sol.desenhar()
-  sol.gravidade(planeta)
+
+  mercurio.desenhar('#6e5707')
+  terra.desenhar('#0206ed')
+  venus.desenhar('#de049d')
   
-  planeta.atulizar()
+  mercurio.atulizar()
+  terra.atulizar()
+  venus.atulizar()
+
+  sol.desenhar('#e1ed02')
+  sol.gravidade(terra)
+  sol.gravidade(mercurio)
+  sol.gravidade(venus)
 }
 
 function Corpo(_massa, _posicao, _velocidade){
@@ -35,16 +47,25 @@ function Corpo(_massa, _posicao, _velocidade){
   this.posicao = _posicao
   this.velocidade = _velocidade
   this.raio = this.massa
+  this.path = []
 
-  this.desenhar = function(){
+  this.desenhar = function(cor){
     noStroke()
-    fill(255)
+    fill(cor)
     ellipse(this.posicao.x, this.posicao.y, this.raio, this.raio)
+    stroke(30)
+    for(let i = 0; i < this.path.length -2; i++){
+      line(this.path[i].x, this.path[i].y, this.path[i+1].x, this.path[i+1].y)
+    }
   }
 
   this.atulizar = function(){
     this.posicao.x += this.velocidade.x
     this.posicao.y += this.velocidade.y
+    this.path.push(this.posicao.copy())
+    if(this.path.length > 50){
+      this.path.splice(0, 1)
+    }
   }
 
   this.aplicarForca = function(forca){
@@ -54,7 +75,7 @@ function Corpo(_massa, _posicao, _velocidade){
 
   this.gravidade = function(planeta){
     let distacia = this.distanciaEntreDoisAstros(this.posicao.x, this.posicao.y, planeta.posicao.x, planeta.posicao.y)
-    let f = {x:this.posicao.x - planeta.posicao.x/distacia , y:this.posicao.y - planeta.posicao.y/distacia}
+    let f = {x:((this.posicao.x - planeta.posicao.x) /distacia) , y:((this.posicao.y - planeta.posicao.y) / distacia)}
     let multMassas = this.massa*planeta.massa
     let multDistacia = distacia*distacia
     let constGravitacional = GRAVIDADE * multMassas / multDistacia
